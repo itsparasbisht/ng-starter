7f84e382309bb050c00e50ad0a2f356d856273a9
itsparasbisht <iamparasbisht@gmail.com>
2023-11-08 15:40:19 +0530

    iterating an array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  items = [new WishItem('Learn Angular'), new WishItem('Learn React')];
   title = 'angular-wishlist';
 }
