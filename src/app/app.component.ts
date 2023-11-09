09fdddbce4be289cd3e2125f8af974dc0300248b
itsparasbisht <iamparasbisht@gmail.com>
2023-11-09 19:25:37 +0530

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { WishItem } from 'src/shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -7,36 +6,8 @@ import { WishItem } from 'src/shared/models/wishItem';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  items: WishItem[] = [
-    new WishItem('Learn Angular'),
-    new WishItem('Learn React', true),
-    new WishItem('Learn Vue'),
-  ];
-
-  listFilter: string = '0';
-
-  newWishText = '';
-
   title = 'angular-wishlist';
-
-  get visibleItems(): WishItem[] {
-    let value = this.listFilter;
-    if (value === '0') {
-      return this.items;
-    } else if (value === '1') {
-      return this.items.filter((item) => !item.isComplete);
-    } else {
-      return this.items.filter((item) => item.isComplete);
-    }
-  }
-
-  addNewWish() {
-    this.items.push(new WishItem(this.newWishText));
-    this.newWishText = '';
-  }
-
-  toggleItem(item: WishItem) {
-    item.isComplete = !item.isComplete;
-    console.log(item);
+  getValue() {
+    return 'JavaScript';
   }
 }
