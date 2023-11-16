5a80baff076d9ac67563352ddbfc56fcde287d1f
itsparasbisht <iamparasbisht@gmail.com>
2023-11-16 19:11:21 +0530

    reference variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = '';
+  getInput(ref: HTMLInputElement) {
+    console.log(ref);
+  }
 }
