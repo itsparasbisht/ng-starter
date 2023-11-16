26abcc667d0b416aa41c97ce332cf8c2b88ae438
itsparasbisht <iamparasbisht@gmail.com>
2023-11-16 19:04:35 +0530

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello';
-  user = 'Guest';
-  updateUser(data: string) {
-    this.user = data;
-  }
+  name = '';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [AppComponent, ChildComponent],
-  imports: [BrowserModule, FormsModule, NgbModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
